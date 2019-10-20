09a7b4f8e6ac63c1b406e45694cb5bcb552216cc
Alex Suthammanont <asuthamm@gmail.com>
2019-10-20 02:09:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,7 +29,8 @@ def array_literal_matrix
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
   
-  arr1 = 
+  arr1 = ENNET_HOUSE.sort
+  arr2 = 
   q([ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort])
 end
 
